921d28ddfb9e197a02d9b31f577a80a879c68484
Xue Zhang <xue.zhang@shopee.com>
2020-07-16 20:01:55 +0800

    Build demo

---
+++ storybook-static/precache-manifest.726c6bb1694c7a6c5b3665ee47dfb1ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "843684f7cb6ab8f12faaf4b7c2701f60",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.1e934b14a5aa58d65079.bundle.js"
+  },
+  {
+    "url": "runtime~main.1e934b14a5aa58d65079.bundle.js"
+  },
+  {
+    "revision": "e186f8c584f61e63e757",
+    "url": "static/css/main.66e08d09.chunk.css"
+  },
+  {
+    "url": "vendors~main.1e934b14a5aa58d65079.bundle.js"
+  },
+  {
+    "url": "vendors~main.1e934b14a5aa58d65079.bundle.js.LICENSE.txt"
+  }
+]);
